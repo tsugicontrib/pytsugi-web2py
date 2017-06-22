12b0360c3592966df3e3a275fc806419a53f5833
Charles Severance <csev@umich.edu>
2017-06-21 21:30:20 -0400

    Change to the simpler variant

--- models/db.py
+++ models/db.py
@@ -32,8 +32,7 @@ if not request.env.web2py_runtime_gae:
     #         pool_size=myconf.get('db.pool_size'),
     #         migrate_enabled=myconf.get('db.migrate'),
     #         check_reserved=['all'])
-    db = DAL('mysql://ltiuser:ltipassword@localhost:8889/tsugi',
-            fake_migrate_all=True)
+    db = DAL('mysql://ltiuser:ltipassword@localhost:8889/tsugi')
 else:
     # ---------------------------------------------------------------------
     # connect to Google BigTable (optional 'google:datastore://namespace')
